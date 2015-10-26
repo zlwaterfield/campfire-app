6e396cb08000661a6d36ae7c290aaad6a01e7aab
Zach Waterfield <zlwaterfield@gmail.com>
2015-10-25 21:30:03 -0400

    Update dictionary

--- src/js/constants/dictionary.js
+++ src/js/constants/dictionary.js
@@ -15,7 +15,7 @@ module.exports = Object.freeze({
   'under 500': 'over=0&under=500',
   'under 1000': 'over=0&under=1000',
   'all prices': 'over=0',
-  'next month': 'after=' + today + '&before=' + '-' + thisMonth + '-31',
+  'next month': 'after=' + today + '&before=' + thisYear + '-'  + thisMonth + '-31'
   'next 3 months': 'after=' + today + '&before=' + thisYear + '-' + threeMonths + '-31',
   'this year': 'after=' + thisYear + '-01-01&before=' + thisYear + '-12-31',
   'next year': 'after=' + nextYear + '-01-01&before=' + nextYear + '-12-31',
